88e1cfbb8ae7e365852ab516b4f30e3f10fd4767
Md Rizve Hasan <62834591+RizveOve@users.noreply.github.com>
2025-04-22 15:09:59 +0200

    small fix

--- src/minor-components/genericBackgroundSubSection.jsx
+++ src/minor-components/genericBackgroundSubSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import RankedImage from "./rankedImage";
 const GenericBackgroundSubSection = ({
   title = "Intrinsic (Saliency Based)",
   text = `Intrinsic explanations visualize which regions of an image the model finds most important for a given prediction. These visualizations are presented as heatmaps, where red indicates important regions and blue indicates non-important regions.`,
@@ -12,6 +13,12 @@ const GenericBackgroundSubSection = ({
 }) => {
   return (
     <div className={className}>
+      <RankedImage
+        path={imagePath}
+        alternativeText={imageAlternativeText}
+        className={imageClassName}
+      />
+
       <div className={descriptionClassName}>
         <span className={titleClassName}>{title}</span>
         <p className={textClassName}> {text}</p>
